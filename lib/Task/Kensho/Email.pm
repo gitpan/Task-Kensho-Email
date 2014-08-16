8fc438374580510729a6aec058c26b59d7b427a7
Karen Etheridge <ether@cpan.org>
2014-08-16 08:29:04 +0000

    Import of ETHER/Task-Kensho-Email-0.37 from CPAN.
    gitpan-cpan-distribution: Task-Kensho-Email
    gitpan-cpan-version:      0.37
    gitpan-cpan-path:         ETHER/Task-Kensho-Email-0.37.tar.gz
    gitpan-cpan-author:       ETHER
    gitpan-cpan-maturity:     released

--- lib/Task/Kensho/Email.pm
+++ lib/Task/Kensho/Email.pm
@@ -1,11 +1,8 @@
 use strict;
 use warnings;
 package Task::Kensho::Email;
-BEGIN {
-  $Task::Kensho::Email::AUTHORITY = 'cpan:PERIGRIN';
-}
-# git description: v0.35-27-gd046b2e
-$Task::Kensho::Email::VERSION = '0.36';
+# git description: v0.36-24-g97e1e22
+$Task::Kensho::Email::VERSION = '0.37';
 # ABSTRACT: A Glimpse at an Enlightened Perl: Email
 
 __END__
@@ -20,7 +17,7 @@ Task::Kensho::Email - A Glimpse at an Enlightened Perl: Email
 
 =head1 VERSION
 
-version 0.36
+version 0.37
 
 =head1 SYNOPSIS
 
@@ -123,5 +120,25 @@ This software is copyright (c) 2008 by Chris Prather.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
+
+=head1 CONTRIBUTORS
+
+=for stopwords Karen Etheridge Leo Lapworth Chris Nehren
+
+=over 4
+
+=item *
+
+Karen Etheridge <ether@cpan.org>
+
+=item *
+
+Leo Lapworth <leo@cuckoo.org>
+
+=item *
+
+Chris Nehren <apeiron@cpan.org>
+
+=back
 
 =cut
